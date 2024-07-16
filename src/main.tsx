d41701e73f6e8b8c0515f53d36c3c2c3d3decb3b
blake <foxtrotpipefake@gmail.com>
2024-07-16 18:46:44 +0800

    Fix route bug

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/football-page' element={<App/>}>
+        <Route path='/' element={<App/>}>
         {/* <Route path='/' element={<App/>}>
           <Route index element={<Home/>}/>
           <Route path='/fav-matches' element={<FavMatches/>}/> */}
